1ac7427218c5d8d1f4c1a0158e722aead5574c62
Luis felipe kaufmann da Silva <luis.kaufmann.silva@gmail.com>
2018-09-07 20:30:33 -0300

    save

--- ProjetoRandomNumber/app/src/main/java/com/kaufmann/projetorandomnumber/MainActivity.kt
+++ ProjetoRandomNumber/app/src/main/java/com/kaufmann/projetorandomnumber/MainActivity.kt
@@ -2,11 +2,20 @@ package com.kaufmann.projetorandomnumber
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickBotao(view: View){
+        val texto = findViewById(R.id.texto) as TextView
+        texto.setText("Texto alterado")
+
     }
 }
