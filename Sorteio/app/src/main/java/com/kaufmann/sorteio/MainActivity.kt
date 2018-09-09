905a65b0d16aeb79269fab5339f34f753a4ee50e
Luis felipe kaufmann da Silva <luis.kaufmann.silva@gmail.com>
2018-09-09 15:50:32 -0300

    s6a21

--- Sorteio/app/src/main/java/com/kaufmann/sorteio/MainActivity.kt
+++ Sorteio/app/src/main/java/com/kaufmann/sorteio/MainActivity.kt
@@ -2,11 +2,19 @@ package com.kaufmann.sorteio
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun randonChoose(view: View){
+        val n = Random().nextInt(11)
+        selected.setText("Número sorteado é: $n")
     }
 }
