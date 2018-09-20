58e199baa8b5eafc73a6fd1d480b75609be3d48f
Luis felipe kaufmann da Silva <luis.kaufmann.silva@gmail.com>
2018-09-20 20:53:48 -0300

    s9a91

--- ATMConsultoria/app/src/main/java/com/kaufmann/atmconsultoria/MainActivity.kt
+++ ATMConsultoria/app/src/main/java/com/kaufmann/atmconsultoria/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kaufmann.atmconsultoria
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,18 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_cliente.setOnClickListener {
+            Toast.makeText(applicationContext, "Abrindo cliente!", Toast.LENGTH_SHORT)
+                    .show()
             startActivity(Intent(this, DetalheClienteActivity::class.java))
         }
 
         btn_contato.setOnClickListener {
+            Toast.makeText(applicationContext, "Abrindo contato!", Toast.LENGTH_SHORT)
+                    .show()
             startActivity(Intent(this, DetalheContatoActivity::class.java))
         }
 
         btn_empresa.setOnClickListener {
+            Toast.makeText(applicationContext, "Abrindo empresa!", Toast.LENGTH_SHORT)
+                    .show()
             startActivity(Intent(this, DetalheEmpresaActivity::class.java))
         }
 
         btn_servico.setOnClickListener {
+            Toast.makeText(applicationContext, "Abrindo servico!", Toast.LENGTH_SHORT)
+                    .show()
             startActivity(Intent(this, DetalheServicosActivity::class.java))
         }
 
