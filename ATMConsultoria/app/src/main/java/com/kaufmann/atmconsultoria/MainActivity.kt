536eded1d1aecddcd648d53b22d61c8658d6cefa
Luis felipe kaufmann da Silva <luis.kaufmann.silva@gmail.com>
2018-09-20 20:50:26 -0300

    s9a90

--- ATMConsultoria/app/src/main/java/com/kaufmann/atmconsultoria/MainActivity.kt
+++ ATMConsultoria/app/src/main/java/com/kaufmann/atmconsultoria/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.kaufmann.atmconsultoria
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_cliente.setOnClickListener {
+            startActivity(Intent(this, DetalheClienteActivity::class.java))
+        }
+
+        btn_contato.setOnClickListener {
+            startActivity(Intent(this, DetalheContatoActivity::class.java))
+        }
+
+        btn_empresa.setOnClickListener {
+            startActivity(Intent(this, DetalheEmpresaActivity::class.java))
+        }
+
+        btn_servico.setOnClickListener {
+            startActivity(Intent(this, DetalheServicosActivity::class.java))
+        }
+
     }
+
+
 }
